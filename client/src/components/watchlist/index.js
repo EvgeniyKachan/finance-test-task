abef91a58042748fce54a286e5da0fe08519337b
Yevhenii Kachan <djianosis@gmail.com>
2021-05-16 13:09:11 +0300

    add redux

---
+++ client/src/components/watchlist/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {connect} from 'react-redux';
+
+function Watchlist(props) {
+
+    return(
+        <div> 
+            <div>
+                test
+            </div> 
+            <div> 
+                test
+            </div>
+        </div>
+
+    )
+}
+
+export default connect()(Watchlist)
---
+++ client/src/store/index.js
@@ -0,0 +1,7 @@
+import {createStore} from 'redux';
+
+function rootReducer(state, action) {
+    return state;
+}
+
+export const store = createStore(rootReducer);
